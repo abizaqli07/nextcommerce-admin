8a5c10cbd13a4ba14e4936730f17fcc5fbeeb7de
abikhoir07 <abikhoir42@gmail.com>
2023-08-05 23:30:47 +0700

    creating store setting

---
+++ src/app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -0,0 +1,42 @@
+import prisma from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { FC } from "react";
+import SettingsForm from "./components/settings_form";
+
+interface SettingsPageprops {
+  params: {
+    storeId: string;
+  }
+}
+
+const SettingsPage: FC<SettingsPageprops> = async ({
+  params
+}) => {
+  const { userId } = auth()
+
+  if (!userId) {
+    redirect("/sign-in")
+  }
+
+  const store = await prisma.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId: userId
+    }
+  })
+
+  if (!store) {
+    redirect("/")
+  }
+
+  return (
+    <div className=" flex-col">
+      <div className=" flex-1 space-y-4 p-8 pt-6">
+        <SettingsForm initialData={store}/>
+      </div>
+    </div>
+  )
+}
+
+export default SettingsPage
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,32 @@
+import { UserButton, auth } from "@clerk/nextjs"
+import MainNav from "@/components/main_nav"
+import StoreSwitcher from "@/components/store_switcher"
+import { redirect } from "next/navigation";
+import prisma from "@/lib/prismadb";
 
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/sign-in")
+  }
+
+  const stores = await prisma.store.findMany({
+    where: {
+      userId: userId
+    }
+  })
+
   return (
-    <div>Navbar</div>
+    <div className=" border-b">
+      <div className=" flex h-16 items-center px-4">
+        <StoreSwitcher items={stores} />
+        <MainNav className=" mx-6" />
+        <div className=" ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </div>
   )
 }
 
